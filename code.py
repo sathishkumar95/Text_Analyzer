17b256180ac8bcf4c0329eb30fa737183f8bb8ec
P SATHISH KUMAR <sathishp.railway@gmail.com>
2017-07-27 15:15:28 +0530

    new change

--- code.py
+++ code.py
@@ -10,4 +10,4 @@ with open('text.txt') as f:
     text = f.read()
     alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     for al in alpha:
-        print("{0} count is {1}", al, counter(text, al))
+        print("%c count is %d" % (al, counter(text, al)))
